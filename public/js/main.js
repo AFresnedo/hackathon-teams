53551925e3b8bbc88c50881cb03d6769f5ff84aa
AFresnedo <AFresnedo@users.noreply.github.com>
2018-08-17 08:00:33 -0700

    change goto edit team from button to anchor

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,4 @@
 $(document).ready(function() {
   console.log('main.js reached on this page');
 
-  // add event listeners for routing buttons
-  $('#edit-team').click(function() {
-    // redirect to edit team route
-  });
 });
