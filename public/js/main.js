df28d4e9bc3ae81efe5227f755d367567c5e3d5f
AFresnedo <AFresnedo@users.noreply.github.com>
2018-08-17 07:33:38 -0700

    begin coding goto edit page btn

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
   console.log('main.js reached on this page');
+
+  // add event listeners for routing buttons
+  $('#edit-team').click(function() {
+    // redirect to edit team route
+  });
 });
